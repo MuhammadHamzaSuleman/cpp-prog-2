6c4e5ed982d23eefd3f699cb155c1a406569a165
Muhammad hamza <102409025+muhammadhamza890@users.noreply.github.com>
2022-05-10 18:52:34 +0500

    cpp programe 2 Add integer

---
+++ Hamza 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"enter two numbers";
+    cin>>num1>>num2;
+    cout<<"the first num is: "<<num1<<endl;
+    cout<<"the second num is: "<<num2<<endl;
+    return 0;
+}
